bac5b66c5ce68887a35faf6226ac74212ddca652
toyijiu <309378714@qq.com>
2017-04-18 23:41:09 +0800

    singleton模式代码实现

---
+++ 设计模式/singleton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wuxiao on 17-4-18.
+//
+
+#include "singleton.h"
---
+++ 设计模式/singleton.h
@@ -0,0 +1,8 @@
+//
+// Created by wuxiao on 17-4-18.
+//
+
+#ifndef DAILYBLOG_SINGLETON_H
+#define DAILYBLOG_SINGLETON_H
+
+#endif //DAILYBLOG_SINGLETON_H
